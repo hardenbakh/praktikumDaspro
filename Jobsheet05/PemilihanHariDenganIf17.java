912f57aee3e3160388297fb1fc0adcf2dce73dde
hardenbakh <muhammadmuqribillah@gmail.com>
2025-10-08 10:10:34 +0700

    modifikasi percobaan 2

---
+++ Jobsheet05/PemilihanHariDenganIf17.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class PemilihanHariDenganIf17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Masukkan angka hari (1-7): ");
+        int angka = sc.nextInt();
+
+        if (angka >= 1 && angka <= 5){
+            System.out.println( "weekday");
+        } else if (angka > 5 && angka <7){
+            System.out.println("weekend");
+        }else {
+            System.out.println("invalid number");
+        }
+    }
+}
