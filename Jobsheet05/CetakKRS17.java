49c2735a585a175b326401abb5f7b27e2bf9cbe3
hardenbakh <muhammadmuqribillah@gmail.com>
2025-10-08 09:08:21 +0700

    Jobsheet05

---
+++ Jobsheet05/CetakKRS17.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+public class CetakKRS17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("--- Cetak KRS SIAKAD ---");
+        System.out.println(" Apakah UKT sudah lunas? (true/false)");
+        boolean uktlunas = sc.nextBoolean();
+
+        if (uktlunas) {
+            System.out.println("Pembayaran UKT Terverifikasi");
+            System.out.println("silahkan setak KRS dan minta tanda tangan DPA");
+        }
+    }
+}
