df10b2436ed6da7eb42e325125cbb6e4f5dac368
hardenbakh <muhammadmuqribillah@gmail.com>
2025-11-16 17:59:21 +0700

    ratanilai modifikasi

--- Jobsheet09/ArrayRataNilai16.java
+++ Jobsheet09/ArrayRataNilai16.java
@@ -5,19 +5,48 @@ public class ArrayRataNilai16 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int [] nilaiMhs = new int[10];
-        int total = 0;
-        double rata2;
+        System.out.println("Masukkan jumlah mahasiswa: ");
+        int JumlahMhs = sc.nextInt();
 
+        int [] nilaiMhs = new int[JumlahMhs];
+
+        int totalLulus = 0, totalTidakLulus = 0;
+        int JumlahLulus = 0, JumlahTidakLulus = 0;
+        double rata2lulus, rata2tidakLulus;
+
+        int max= 0;
+        int min= 100;
+
+
+        
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
             nilaiMhs[i] = sc.nextInt();
+
+            if (nilaiMhs[i] >= 70) {
+                totalLulus += nilaiMhs[i];
+                JumlahLulus++;
+            } else {
+                totalTidakLulus += nilaiMhs[i];
+                JumlahTidakLulus++;
+            }
+             
+            if (nilaiMhs[i] > max) {
+                max = nilaiMhs[i];
+            }
+            if (nilaiMhs[i]< min) {
+                min = nilaiMhs[i];
+            }
         }
-        for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
-        }
-        rata2 = total / nilaiMhs.length;
-        System.out.println("Rata-rata nilai mahasiswa adalah: " + rata2);
+        
+        
+        rata2lulus = totalLulus/ JumlahLulus;
+        rata2tidakLulus = totalTidakLulus / JumlahTidakLulus;
+
+        System.out.println("Rata2 mahasiswa yang lulus: " + rata2lulus);
+        System.out.println("Rata2 mahasiswa yang tidak lulus: " + rata2tidakLulus);
+        System.out.println("Nilai tertinggi: " + max);
+        System.out.println("Nilai terendah: " + min);
     }
     
 }
