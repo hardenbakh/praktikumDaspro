6facdb8b1ea44c87a7f19addde0936fde0d858cf
hardenbakh <muhammadmuqribillah@gmail.com>
2025-11-16 18:42:51 +0700

    search nilai

--- Jobsheet09/SearchNilai16.java
+++ Jobsheet09/SearchNilai16.java
@@ -1,21 +1,34 @@
 package Jobsheet09;
-
+import java.util.Scanner;
 public class SearchNilai16 {
 
     public static void main(String[] args) {
-     int [] nilai = {80, 85, 78, 96, 90, 82, 86};
-     int key = 90;   
-     int hasil=0;
+        Scanner sc = new Scanner(System.in);
+     
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int JumlahMhs = sc.nextInt();
 
-     for (int i = 0; i < nilai.length; i++) {
-        if (nilai[i] == key) {
-            hasil = i;
-            break;
+        int [] nilaiMhs = new int[JumlahMhs];
+
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            nilaiMhs[i] = sc.nextInt();
         }
-     }
-     System.out.println();
-     System.out.println("Nilai " + key + " ditemukan pada index ke-" + hasil);
-     System.out.println();
+
+        System.out.print("Masukkan nilai yang dicari: ");
+        int cariNilai = sc.nextInt();
+        boolean ditemukan = false;
+
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            if (nilaiMhs[i] == cariNilai) {
+                System.out.println("Nilai " + cariNilai + " ditemukan pada indeks ke-" + i);
+                ditemukan = true;
+            } 
+
     }
-    
+    if (!ditemukan) {
+        System.out.println();
+        System.out.println("nilai yang dicari tidak ditemukan");
+    }
+}
 }
