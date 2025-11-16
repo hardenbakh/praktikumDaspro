67d33d2279b3b8c3330bdc91ccbf3a4b03c7355b
hardenbakh <muhammadmuqribillah@gmail.com>
2025-11-16 14:21:11 +0700

    arrayBilangan

--- Jobsheet09/ArrayBilangan16.java
+++ Jobsheet09/ArrayBilangan16.java
@@ -3,6 +3,7 @@ package Jobsheet09;
 public class ArrayBilangan16 {
     
     public static void main(String[] args) {
+
         int [] bil = new int[4];
 
         bil[0]=5;
@@ -10,9 +11,8 @@ public class ArrayBilangan16 {
         bil[2]=-7;
         bil[3]=17;
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+       for (int i =0; i<=4;i++){
+        System.out.println(bil[i]);
+       }
     }
 }
