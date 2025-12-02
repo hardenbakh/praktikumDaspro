19aa09f592fafa41e7f7f37a7d736a6e3fb44d0d
hardenbakh <muhammadmuqribillah@gmail.com>
2025-12-02 19:36:31 +0700

    kafe 16 modifikasi

--- Pertemuan14/Kafe16.java
+++ Pertemuan14/Kafe16.java
@@ -1,12 +1,77 @@
 package Pertemuan14;
-
+import java.util.Scanner;
 public class Kafe16 {
 
     public static void main(String[] args) {
-        Menu();
+        Menu("Budi", true, "DISKON50,DISKON30");
+
+        Scanner sc = new Scanner(System.in);
+
+        // memasukkan nama menu dan banyak item 
+        System.out.println("masukkan nomor menu : ");
+        int pilihanMenu = sc.nextInt();
+
+        System.out.println("masukkan banyak item :");
+        int banyakItem = sc.nextInt();
+
+        sc.nextLine();
+
+        System.out.println("Masukkan kode promo : ");
+        String kodePromo = sc.nextLine();
+
+
+        int totalBayar = hitungTotalHarga16(pilihanMenu, banyakItem, kodePromo);
+
+        System.out.println("total yang harus dibayarkan " + totalBayar);
+
     }
+
+    // perhitungan harga
+    public static int hitungTotalHarga16(int pilihanMenu, int banyakItem, String kodePromo){
+        int[] hargaItems ={15000,20000,22000,12000,10000,18000};
+
+        int hargaTotal = hargaItems[pilihanMenu-1] * banyakItem;
+
+        int diskon = 0;
+        if (kodePromo.equalsIgnoreCase("DISKON50")) {
+            diskon = hargaTotal * 50/100;
+            System.out.println("Selamat anda mendapat pengurangan harga sebesar 50%");
+
+        } else if (kodePromo.equalsIgnoreCase("DISKON30")) {
+            diskon = hargaTotal * 30/100;
+            System.out.println("Selamat anda mendapat pengurangan harga sebesar 30%");
+
+        } else  {
+            System.out.println(" karena kode invalid maka tidak ada pengurangan total harga!");
+        }
+
+        hargaTotal -= diskon;
+
+        return hargaTotal;
+    }
+
     
-    public static void Menu(){
+    public static void Menu(String namaPelanggan, boolean isMember, String kodePromo){
+        Scanner sc = new Scanner(System.in);
+        // menampilkan nama pelanggan
+        System.out.println("Selamat datang, " + namaPelanggan + "!");
+        // menampilkan member jika nilainya true
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+        // memasukkan kode promo
+        // System.out.println("Masukkan kode promo anda : ");
+        // kodePromo = sc.nextLine();
+        // // menampilkan kode promo yang didapat
+        // if (kodePromo.equalsIgnoreCase("DISKON50")) {
+        //     System.out.println("Selamat anda mendapatkan diskon sebesar 50%");
+
+        // } else if (kodePromo.equalsIgnoreCase("DISKON30")) {
+        //     System.out.println("Selamat anda mendapatkan diskon sebesar 30%");
+        // }else   {
+        //     System.out.println("kode promo tidak valid!!");
+        // }
+        // daftar menu
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
@@ -16,6 +81,7 @@ public class Kafe16 {
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("===========================");
         System.out.println("Silahkan pilih menu yang Anda inginkan.");
-        System.err.println();
     }
+
+    
 }
